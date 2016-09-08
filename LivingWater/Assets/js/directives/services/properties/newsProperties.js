bee3cf6360b0b70a229a25f320163f8be0161cb3
ljbdelacruz123 <ljbdelacruz123@gmail.com>
2016-09-08 18:56:55 +0800

    made some changes

--- LivingWater/Assets/js/directives/services/properties/newsProperties.js
+++ LivingWater/Assets/js/directives/services/properties/newsProperties.js
@@ -10,6 +10,16 @@
             id: 2, title: 'Headline 2', content: 'This Just In',
             images: [{ id: 2, image: '/Assets/images/etc/img1.jpg', page_id: 2, width: 300, height: 300 }],
             videos: [{ id: 1, news_id: 1, title: 'Youtube video player', design: 'youtube-player', type: 'text/html', width: 300, height: 300, source: 'https://www.youtube.com/embed/ZL6BP8ss-5Q', thumbnail: 'http://img.youtube.com/vi/ZL6BP8ss-5Q/default.jpg', frameborder: 0 }]
+        },
+        {
+            id: 3, title: 'Headline 3', content: 'This Just In',
+            images: [{ id: 2, image: '/Assets/images/etc/img3.jpg', page_id: 2, width: 300, height: 300 }],
+            videos: [{ id: 1, news_id: 1, title: 'Youtube video player', design: 'youtube-player', type: 'text/html', width: 300, height: 300, source: 'https://www.youtube.com/embed/ZL6BP8ss-5Q', thumbnail: 'http://img.youtube.com/vi/ZL6BP8ss-5Q/default.jpg', frameborder: 0 }]
+        },
+        {
+            id: 4, title: 'Headline 4', content: 'This Just In',
+            images: [{ id: 2, image: '/Assets/images/etc/img4.jpg', page_id: 2, width: 300, height: 300 }],
+            videos: [{ id: 1, news_id: 1, title: 'Youtube video player', design: 'youtube-player', type: 'text/html', width: 300, height: 300, source: 'https://www.youtube.com/embed/ZL6BP8ss-5Q', thumbnail: 'http://img.youtube.com/vi/ZL6BP8ss-5Q/default.jpg', frameborder: 0 }]
         }
         ],
     };
