068d7409579d5329ef982e9d1fb33af1e4cb60fc
ljbdelacruz123 <ljbdelacruz123@gmail.com>
2016-10-05 07:07:07 +0800

    changes today

---
+++ LivingWater/Assets/js/directives/template1/house/transaction/module.js
@@ -0,0 +1,13 @@
+﻿angular.module('modules.Transaction', [])
+.config(['$routeProvider',
+         function ($routeProvider) {
+             /* Handle route on location path is '/' */
+             $routeProvider.when('/Transaction', {
+                 templateUrl: '/Assets/js/directives/template1/house/transaction/transaction.html',
+                 controller: 'transactionCtrl'
+             });
+         }
+]);
+
+
+
---
+++ LivingWater/Assets/js/directives/template1/house/transaction/transactionCtrl.js
@@ -0,0 +1,20 @@
+﻿angular.module('modules.ViewProducts')
+.controller('viewProductsCtrl',
+            ['$scope',
+             'productService',
+             'updateProduct',
+             'routeChecker',
+             function ($scope, productService, updateProduct, routeChecker) {
+                 $scope.productTypes = productService.products;
+                 $scope.productEdit = productService.productSelected;
+                 $scope.viewProduct = false;
+                 $scope.editProduct = true;
+                 $scope.saveChanges_OnClicked = function () {
+                     updateProduct($scope.productEdit, $scope.finishedUpdate);
+                 };
+                 $scope.finishedUpdate = function () {
+                     alert("Success");
+                 };
+
+             }
+            ]);
